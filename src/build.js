64df28000196beeda65ce155f235bc0656b06262
Jeff Barczewski <jeff.barczewski@gmail.com>
2012-02-08 15:46:02 -0600

    add devDependency for pegjs parser and adjust output for AMD

--- src/build.js
+++ src/build.js
@@ -5,8 +5,18 @@ var peg  = require('pegjs'),
 
 var parser = peg.buildParser(fs.readFileSync(path.join(root, 'src', 'dust.pegjs'), 'utf8'));
 
-fs.writeFileSync(path.join(root, 'lib', 'parser.js'), "(function(dust){\n\nvar parser = "
-  + parser.toSource().replace('this.SyntaxError', 'SyntaxError') + ";\n\n"
-  + "dust.parse = parser.parse;\n\n"
-  + "})(typeof exports !== 'undefined' ? exports : window.dust);"
+fs.writeFileSync(path.join(root, 'lib', 'parser.js'),
+  "if (typeof define !== 'function') { var define = require('../vendor/amdefine')(module); }\n\n" +
+  "define(['./dust'], function (dust) {\n\n" +
+  "var parser = " +
+  parser.toSource().replace('this.SyntaxError', 'SyntaxError') + ";\n\n" +
+  "dust.parse = parser.parse;\n\n" +
+  "  return dust;\n});"
 );
+
+
+
+
+
+
+
